98e1c92587e22b21d7a8230804edca594fb7d2b7
Mattias Ahle <mattias.ahle@gmail.com>
2021-12-27 21:08:39 +0100

    SensorDemo working - MQTT App working and sending data to GCP

--- lab4/given_code/CloudIoTCoreMQTTExample-master/CloudIoTCoreMQTTExample-master/app/src/main/java/com/blundell/tut/MainActivity.java
+++ lab4/given_code/CloudIoTCoreMQTTExample-master/CloudIoTCoreMQTTExample-master/app/src/main/java/com/blundell/tut/MainActivity.java
@@ -1,6 +1,10 @@
 package com.blundell.tut;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
@@ -9,10 +13,14 @@ import com.blundell.iotcore.IotCoreCommunicator;
 
 import java.util.concurrent.TimeUnit;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
 
     private IotCoreCommunicator communicator;
     private Handler handler;
+
+    private long previousSamplingTimeAcc, previousSamplingTimeMag, previousSamplingTimeGyr;
+    private int samplingPeriod = 10;      //default sampling period
+    private float accX, accY, accZ, magX, magY, magZ, gyrX, gyrY, gyrZ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +40,9 @@ public class MainActivity extends Activity {
         thread.start();
         handler = new Handler(thread.getLooper());
         handler.post(connectOffTheMainThread); // Use whatever threading mechanism you want
+
+        setupSamplingControls();
+        setupSensorListeners();
     }
 
     private final Runnable connectOffTheMainThread = new Runnable() {
@@ -44,26 +55,88 @@ public class MainActivity extends Activity {
     };
 
     private final Runnable sendMqttMessage = new Runnable() {
-        private int i;
+//        private int i;
 
         /**
          * We post 100 messages as an example, 1 a second
          */
         @Override
         public void run() {
-            if (i == 100) {
-                return;
-            }
+//            if (i == 100) {
+//                return;
+//            }
 
             // events is the default topic for MQTT communication
             String subtopic = "events";
             // Your message you want to send
-            String message = "Hello World " + i++;
+//            String message = "Hello World " + i++;
+            String message = "acc:\t" + accX + "\t" + accY + "\t" + accZ + "\n" +
+                    "mag:\t" + magX + "\t" + magY + "\t" + magZ + "\n" +
+                    "gyr:\t" + gyrX + "\t" + gyrY + "\t" + gyrZ;
             communicator.publishMessage(subtopic, message);
 
-            handler.postDelayed(this, TimeUnit.SECONDS.toMillis(1));
+            handler.postDelayed(this, TimeUnit.SECONDS.toMillis(5));
         }
     };
+
+
+    private void setupSamplingControls() {
+        double samplingFrequency = 0.2;
+        samplingPeriod = (int) (1000 / samplingFrequency);
+        previousSamplingTimeAcc = System.currentTimeMillis();
+        previousSamplingTimeMag = System.currentTimeMillis();
+        previousSamplingTimeGyr = System.currentTimeMillis();
+    }
+
+    private void setupSensorListeners() {
+        SensorManager sm = (SensorManager) this.getSystemService(SENSOR_SERVICE);
+
+        Sensor accelerometer = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        Sensor magnetometer = sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        Sensor gyroscope = sm.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        sm.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sm.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sm.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if ((sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) &&
+                ((System.currentTimeMillis() - previousSamplingTimeAcc) > samplingPeriod)) {
+            accX = sensorEvent.values[0];
+            accY = sensorEvent.values[1];
+            accZ = sensorEvent.values[2];
+            previousSamplingTimeAcc = System.currentTimeMillis();
+
+            System.out.println("MyACC: " + previousSamplingTimeAcc + "\t" + accX + "\t" + accY + "\t" + accZ); // Logcat
+        }
+
+        if ((sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) &&
+                ((System.currentTimeMillis() - previousSamplingTimeMag) > samplingPeriod)) {
+            magX = sensorEvent.values[0];
+            magY = sensorEvent.values[1];
+            magZ = sensorEvent.values[2];
+            previousSamplingTimeMag = System.currentTimeMillis();
+
+            System.out.println("MyMAG: " + previousSamplingTimeMag + "\t" + magX + "\t" + magY + "\t" + magZ); // Logcat
+        }
+
+        if ((sensorEvent.sensor.getType() == Sensor.TYPE_GYROSCOPE) &&
+                ((System.currentTimeMillis() - previousSamplingTimeGyr) > samplingPeriod)) {
+            gyrX = sensorEvent.values[0];
+            gyrY = sensorEvent.values[1];
+            gyrZ = sensorEvent.values[2];
+            previousSamplingTimeGyr = System.currentTimeMillis();
+
+            System.out.println("MyGYR: " + previousSamplingTimeGyr + "\t" + gyrX + "\t" + gyrY + "\t" + gyrZ); // Logcat
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        ;
+    }
 
     @Override
     protected void onDestroy() {
--- lab4/given_code/SensorDemoDec20/SensorDemoDec20/app/src/main/java/com/example/sensordemodec20/MainActivity.java
+++ lab4/given_code/SensorDemoDec20/SensorDemoDec20/app/src/main/java/com/example/sensordemodec20/MainActivity.java
@@ -5,86 +5,76 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.hardware.SensorManager;
 import android.hardware.Sensor;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    public TextView myTxt1;
-    public Button myBtn1;
-    private int globalVar = 0;
-    private SensorManager sensorManager;
-    private Sensor accelero;
-    private long previousSamplingTime;
+    private long previousSamplingTimeAcc, previousSamplingTimeMag, previousSamplingTimeGyr;
     private int samplingPeriod = 10;      //default samling period
-    private final double samplingFrequency = 0.2;    //sampling frequency 0.2Hz
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setupControls();
+        setupSamplingControls();
+        setupSensorListeners();
     }
 
-    private void setupControls() {
+    private void setupSensorListeners() {
+        SensorManager sm = (SensorManager) this.getSystemService(SENSOR_SERVICE);
+
+        Sensor accelerometer = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        Sensor magnetometer = sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        Sensor gyroscope = sm.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        sm.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sm.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sm.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private void setupSamplingControls() {
+        double samplingFrequency = 0.2;
         samplingPeriod = (int) (1000 / samplingFrequency);
-        previousSamplingTime = System.currentTimeMillis();
-        myTxt1 = (TextView) findViewById(R.id.mytxt);
-        myBtn1 = (Button) findViewById(R.id.mybtn);
+        previousSamplingTimeAcc = System.currentTimeMillis();
+        previousSamplingTimeMag = System.currentTimeMillis();
+        previousSamplingTimeGyr = System.currentTimeMillis();
     }
 
-    public void buttonClicked(View view) {
-        switch (view.getId()) {
-            case R.id.mybtn:
-                if (globalVar == 0) myTxt1.setText("Accelerometer");
-                if (globalVar == 1) myTxt1.setText("Magnetometer");
-                if (globalVar == 2) myTxt1.setText("Gyrometer");
-                globalVar++;
-                if (globalVar > 2) globalVar = 0;
-        }
-    }
-
-    //@Override
+    @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        if ((sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) && ((System.currentTimeMillis() - previousSamplingTime) > samplingPeriod)) {
-            float xValue = sensorEvent.values[0];
-            float yValue = sensorEvent.values[1];
-            float zValue = sensorEvent.values[2];
-            previousSamplingTime = System.currentTimeMillis();      //Finlly, save most recent sampling instant.
+        if ((sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) &&
+                ((System.currentTimeMillis() - previousSamplingTimeAcc) > samplingPeriod)) {
+            float accX = sensorEvent.values[0];
+            float accY = sensorEvent.values[1];
+            float accZ = sensorEvent.values[2];
+            previousSamplingTimeAcc = System.currentTimeMillis();
 
-            myTxt1.setText("Accel x = " + xValue);
-
-            System.out.println("Accelerometer change" + " X,Y,Z: " + xValue + "," + yValue + "," + zValue); //for debugging purposes
-
+            System.out.println("ACC: " + previousSamplingTimeAcc + "\t" + accX + "\t" + accY + "\t" + accZ); // Logcat
         }
-//        System.out.println("Time: previous = " + previousSamplingTime); //for debugging purposes
 
-/*
-     repeat for gyro
-           //else if ((sensorEvent.sensor.getType() == Sensor.TYPE_GYROSCOPE) && ((System.currentTimeMillis() - previousSamplingTime) > samplingPeriod)) {
-           //...
-    repeat for magnetometer
-        //else if ((sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) && ((System.currentTimeMillis() - previousSamplingTime) > samplingPeriod)) {
-        //...
+        if ((sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) &&
+                ((System.currentTimeMillis() - previousSamplingTimeMag) > samplingPeriod)) {
+            float magX = sensorEvent.values[0];
+            float magY = sensorEvent.values[1];
+            float magZ = sensorEvent.values[2];
+            previousSamplingTimeMag = System.currentTimeMillis();
 
-*/
+            System.out.println("MAG: " + previousSamplingTimeMag + "\t" + magX + "\t" + magY + "\t" + magZ); // Logcat
+        }
+
+        if ((sensorEvent.sensor.getType() == Sensor.TYPE_GYROSCOPE) &&
+                ((System.currentTimeMillis() - previousSamplingTimeGyr) > samplingPeriod)) {
+            float gyrX = sensorEvent.values[0];
+            float gyrY = sensorEvent.values[1];
+            float gyrZ = sensorEvent.values[2];
+            previousSamplingTimeGyr = System.currentTimeMillis();
+
+            System.out.println("GYR: " + previousSamplingTimeGyr + "\t" + gyrX + "\t" + gyrY + "\t" + gyrZ); // Logcat
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
         ;
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensorManager.registerListener(this,
-                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                SensorManager.SENSOR_DELAY_NORMAL);
-        //    sensorManager.registerListener(this, accelero, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), sensorManager.SENSOR_DELAY_GAME);
-        //      sensorManager.registerListener((SensorEventListener) this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), sensorManager.SENSOR_DELAY_GAME);
     }
 }
